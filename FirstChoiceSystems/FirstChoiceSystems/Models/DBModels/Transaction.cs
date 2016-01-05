f261c58b2effda7638f690b8767c0daaec3644dc
Justin Wyatt <justin@wyattcorporation.com>
2016-01-05 09:16:08 -0600

    hard coded enum values, added missing file.

---
+++ FirstChoiceSystems/FirstChoiceSystems/Models/DBModels/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstChoiceSystems.Models
+{
+    public enum TransactionStatus
+    {
+        Pending =0,
+        Approved =1,
+        Voided =2,
+    }
+    public class Transaction : Entity
+    {
+        public Business Seller { get; set; }
+        public Business Buyer { get; set; }
+        public int Amount { get; set; }
+        public DateTime? ApprovalDate { get; set; }
+        public string Description { get; set; }
+        public TransactionStatus Status { get; set; }
+    }
+}
