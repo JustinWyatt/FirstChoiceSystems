4536d1e3eef86deaea1707b51d0f789a539737ac
Justin Wyatt <justin@wyattcorporation.com>
2016-01-13 10:09:53 -0600

    Successfully added home page

--- FirstChoiceSystems/FirstChoiceSystems/AngularJS/App/app.js
+++ FirstChoiceSystems/FirstChoiceSystems/AngularJS/App/app.js
@@ -1,17 +1,47 @@
-﻿var app = angular.module("app", ['ngRoute', 'ngAnimate']);
+﻿var app = angular.module("app", ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/account/portal",
         {
             controller: 'DashboardController',
-            templateUrl: '/AngularViews/test.html'
+            templateUrl: '/AngularViews/screentest.html'
         })
 
         .when("/item/:id",
         {
             controller: 'itemController',
             templateUrl: '/AngularViews/itemdetails.html'
+        })
+
+        .when("/reports",
+        {
+            templateUrl: '/AngularViews/screentest.html'
+        })
+
+        .when("/marketplace",
+        {
+            templateUrl: '/AngularViews/screentest.html'
+        })
+
+        .when("/businessdirectory",
+        {
+            templateUrl: '/AngularViews/screentest.html'
+        })
+
+        .when("/servicedirectory",
+        {
+            templateUrl: '/AngularViews/screentest.html'
+        })
+
+        .when("/shoppingcart",
+        {
+            templateUrl: '/AngularViews/screentest.html'
+        })
+
+        .when("/account",
+        {
+            templateUrl: '/AngularViews/screentest.html'
         })
 
         .otherwise({ redirectTo: '/account/portal' });
